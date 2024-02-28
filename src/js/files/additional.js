737675e572e0a6bee04ffac38e3703ccae01fbe7
Oleg Fedorov <akademuk24@gmail.com>
2024-02-28 18:18:39 +0200

    add blog section

--- src/js/files/additional.js
+++ src/js/files/additional.js
@@ -2,6 +2,7 @@ export function additional() {
 // Викликати функцію для налаштування FAQ
 	setupFAQ();
 	seo();
+	blogSliders()
 
 	function setupFAQ() {
 		var faqBox = $('.faq__box');
@@ -43,6 +44,34 @@ export function additional() {
 			var buttonText = $(this).hasClass('active') ? 'СВЕРНУТЬ' : 'РАЗВЕРНУТЬ';
 			$(this).find('.btn-text').text(buttonText);
 		});
+	}
+
+	function blogSliders() {
+		let blog = new Swiper('.blog__main', {
+			slidesPerView: 'auto',
+			spaceBetween: 48,
+			loop: true,
+			speed: 500,
+			pagination: {
+				el: '.blog__navigation .swiper-pagination',
+				type: 'fraction',
+				formatFractionCurrent: function(number) {
+					return ('0' + number).slice(-2)
+				},
+				formatFractionTotal: function(number) {
+					return ('0' + number).slice(-2)
+				},
+				renderFraction: function(currentClass, totalClass) {
+					return '<span class="' + currentClass + '"></span>' +
+						'/' +
+						'<span class="' + totalClass + '"></span>'
+				}
+			},
+			navigation: {
+				nextEl: '.blog__navigation .swiper-button-next',
+				prevEl: '.blog__navigation .swiper-button-prev'
+			}
+		})
 	}
 }
 
