9cea3b35c021814711b7a7e0b91b0b8ec77a6d17
Khrapov Denys <khrapov.denys@gmail.com>
2024-02-28 12:56:28 +0200

    added popular obj section and styles, swiper base styles

--- src/js/files/main.js
+++ src/js/files/main.js
@@ -1,3 +1,31 @@
 export function main() {
+	initSwiper()
 
+	function initSwiper() {
+		let popularObj = new Swiper('.popular-obj-slider', {
+			slidesPerView: 'auto',
+			spaceBetween: 48,
+			loop: true,
+			speed: 500,
+			pagination: {
+				el: '.popular-obj__nav .swiper-pagination',
+				type: 'fraction',
+				formatFractionCurrent: function(number) {
+					return ('0' + number).slice(-2)
+				},
+				formatFractionTotal: function(number) {
+					return ('0' + number).slice(-2)
+				},
+				renderFraction: function(currentClass, totalClass) {
+					return '<span class="' + currentClass + '"></span>' +
+						'/' +
+						'<span class="' + totalClass + '"></span>'
+				}
+			},
+			navigation: {
+				nextEl: '.popular-obj__nav .swiper-button-next',
+				prevEl: '.popular-obj__nav .swiper-button-prev'
+			}
+		})
+	}
 }
