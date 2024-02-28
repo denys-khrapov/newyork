3369f74f7fe887095a2ba810b29614b59887fc5d
Oleg Fedorov <akademuk24@gmail.com>
2024-02-28 14:42:01 +0200

    add seo section and update faq section

--- src/js/files/additional.js
+++ src/js/files/additional.js
@@ -1,23 +1,20 @@
 export function additional() {
 // Викликати функцію для налаштування FAQ
 	setupFAQ();
-
+	seo();
 
 	function setupFAQ() {
-		var faqItems = $('.faq__item');
+		var faqBox = $('.faq__box');
 		var showMoreBtn = $('#showMoreBtn');
-		var hiddenItems = faqItems.slice(5);
 		var isExpanded = false; // Track the state for showMoreBtn
-
-		// Сховати всі блоки, крім перших 5
-		hiddenItems.hide();
 
 		// Додати подію на кнопку "Показати більше"
 		showMoreBtn.click(function () {
-			hiddenItems.slideToggle(300); // Adjust the duration as needed
+			faqBox.slideToggle(300); // Adjust the duration as needed
 			isExpanded = !isExpanded;
+
 			var buttonText = isExpanded ? 'СВЕРНУТЬ' : 'РАЗВЕРНУТЬ';
-			$(this).toggleClass('active', isExpanded).text(buttonText);
+			$(this).toggleClass('active', isExpanded).find('.btn-text').text(buttonText);
 		});
 
 		$('.faq__header').click(function () {
@@ -27,20 +24,25 @@ export function additional() {
 			// Toggle 'active' class on clicked header
 			faqHeader.toggleClass('active');
 
-			// Toggle 'active' class on other headers within the same item
-			faqItem.siblings().find('.faq__header').removeClass('active');
+			// Toggle 'active' class on other headers within the document
+			$('.faq__header').not(faqHeader).removeClass('active');
 
-			// Toggle 'active' class on other items
-			faqItem.siblings().removeClass('active').find('.faq__content').slideUp(300);
+			// Toggle 'active' class on other items within the document
+			$('.faq__item').not(faqItem).removeClass('active').find('.faq__content').slideUp(300);
 
 			// Toggle 'active' class on clicked item and toggle content visibility
 			faqItem.toggleClass('active').find('.faq__content').slideToggle(300);
 		});
+
 	}
 
-
-
-
-
+	function seo() {
+		$('#seo__btn').click(function () {
+			$('.seo__main').slideToggle(300);
+			$(this).toggleClass('active');
+			var buttonText = $(this).hasClass('active') ? 'СВЕРНУТЬ' : 'РАЗВЕРНУТЬ';
+			$(this).find('.btn-text').text(buttonText);
+		});
+	}
 }
 
