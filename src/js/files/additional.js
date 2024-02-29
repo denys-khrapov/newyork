0353beb5ce361b7e6edee269cb753e531ff9a593
Oleg Fedorov <akademuk24@gmail.com>
2024-02-29 14:47:52 +0200

    add feedback section

--- src/js/files/additional.js
+++ src/js/files/additional.js
@@ -2,7 +2,9 @@ export function additional() {
 // Викликати функцію для налаштування FAQ
 	setupFAQ();
 	seo();
-	blogSliders()
+	blogSliders();
+	feedback();
+	feedbackSliders()
 
 	function setupFAQ() {
 		var faqBox = $('.faq__box');
@@ -73,5 +75,57 @@ export function additional() {
 			}
 		})
 	}
+
+	function feedback() {
+		$('.feedback__btn').click(function () {
+			var targetBlock = $(this).data('target');
+			var $textElement = $('.feedback__item-top-text[data-block="' + targetBlock + '"]');
+
+			$textElement.toggleClass('expanded'); // Add or remove 'expanded' class
+
+			var isExpanded = $textElement.hasClass('expanded');
+			var buttonText = isExpanded ? 'СВЕРНУТЬ' : 'РАЗВЕРНУТЬ';
+
+			$(this).find('.btn-text').text(buttonText);
+
+			// Add or remove 'active' class on the button
+			$(this).toggleClass('active', isExpanded);
+
+			if (isExpanded) {
+				$textElement.css('-webkit-line-clamp', 'unset'); // Remove line clamp
+			} else {
+				$textElement.css('-webkit-line-clamp', '6'); // Apply line clamp
+			}
+		});
+	}
+
+	function feedbackSliders() {
+		let blog = new Swiper('.feedback__slider', {
+			slidesPerView: 'auto',
+			spaceBetween: 48,
+			loop: true,
+			speed: 500,
+			pagination: {
+				el: '.feedback__navigation .swiper-pagination',
+				type: 'fraction',
+				formatFractionCurrent: function(number) {
+					return ('0' + number).slice(-2)
+				},
+				formatFractionTotal: function(number) {
+					return ('0' + number).slice(-2)
+				},
+				renderFraction: function(currentClass, totalClass) {
+					return '<span class="' + currentClass + '"></span>' +
+						'/' +
+						'<span class="' + totalClass + '"></span>'
+				}
+			},
+			navigation: {
+				nextEl: '.feedback__navigation .swiper-button-next',
+				prevEl: '.feedback__navigation .swiper-button-prev'
+			}
+		})
+	}
+
 }
 
